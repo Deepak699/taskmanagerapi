89f44ae37d766be9d35b6d07b3bc07a16062004a
Deepak <nathanideepak7@gmail.com>
2021-06-20 11:54:02 +0530

    Init commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('./db/mongoose')
+const Users = require('./model/user')
+const Tasks = require('./model/task')
+const UserRouter  = require('./router/user_router')
+const TaskRouter  = require('./router/task_router')
+const app = express()
+const port = process.env.PORT 
+
+// app.use((req,res,next)=>{
+//     res.status('503').send("Site is under maintainence.Try Back soon")
+// })
+
+app.use(express.json())
+app.use(UserRouter)
+app.use(TaskRouter)
+
+
+app.listen(port,()=>{
+    console.log("Server is up"+ port)
+})
